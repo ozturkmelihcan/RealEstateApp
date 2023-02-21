deb0c7f008547b6e9cba60da4bff5b6313684e50
ozturkmelihcan <melihcannozturkk@gmail.com>
2023-02-22 01:34:40 +0300

    v2

--- src/main/java/com/melihcan/realestateapp/controller/restapi/CityController.java
+++ src/main/java/com/melihcan/realestateapp/controller/restapi/CityController.java
@@ -1,4 +1,14 @@
 package com.melihcan.realestateapp.controller.restapi;
 
+import com.melihcan.realestateapp.service.CityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/city")
+@RequiredArgsConstructor
 public class CityController {
+
+    private final CityService cityService;
 }
